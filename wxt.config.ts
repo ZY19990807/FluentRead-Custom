57cb1e49a57161bb22d5aa732683f0fa16e07fdf
ZY19990807 <yourcutepepper@gmail.com>
2025-09-11 21:16:22 +0800

    Initial commit: Fork from FluentRead for custom development

---
+++ wxt.config.ts
@@ -0,0 +1,28 @@
+import {defineConfig} from 'wxt';
+import vue from '@vitejs/plugin-vue';
+import {resolve} from 'path';
+import fs from 'fs';
+
+
+const packageJson = JSON.parse(fs.readFileSync(resolve(__dirname, 'package.json'), 'utf-8'));
+
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+    modules: ['@wxt-dev/webextension-polyfill'],
+    imports: {
+        addons: {
+            vueTemplate: true,
+        },
+    },
+    vite: () => ({
+        plugins: [vue()],
+        define: {
+            'process.env.VUE_APP_VERSION': JSON.stringify(packageJson.version),
+        }
+    }),
+    manifest: {
+        permissions: ['storage', 'contextMenus', 'offscreen'],
+    },
+
+});
